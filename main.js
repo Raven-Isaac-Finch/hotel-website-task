f8adc4f86143d09edbb7db5a102c7de4aaa33f6b
Raven <purplebox02@gmail.com>
2022-09-03 09:27:41 +0300

    JS Added. Project Completed

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// ------------------------------
 // ----- POP-UP DETAIL PAGE -----
 // ---------- OPEN
+// ------------------------------
 const detailsPopUp = document.querySelectorAll('.show-details');
 const cloud = document.querySelector('.cloud');
 const popUpPage = document.querySelector('.details-popup');
@@ -22,7 +24,9 @@ xmark.addEventListener('click', function() {
     cloud.classList.remove('visible');
 });
 
-// ----- SLIDES -----
+// ------------------------------
+// ----- PAGE SLIDE -----
+// ------------------------------
 const dotHolder = document.querySelector('.dot-holder');
 const sliderItems = document.querySelector('.slider__items');
 const slides = document.querySelector('.slider__items').children;
@@ -45,11 +49,10 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 function handleSlider() {
-    // ----- SLIDES
     let slider = images.map(function(image) {
         return `
             <div class="item">
-                <img src="${image}">
+                <img src="${image}"/>
             </div>
         `
     });
@@ -65,12 +68,86 @@ function handleSlider() {
 function handleDots(dotCount) {
     for(let i = 0; i < dotCount; i++) {
         let dot = document.createElement('div');
-        dot.className = 'slider__slide-dots';
+        if(i === 0) {
+            dot.className = 'slider__slide-dots active-dot';
+        } else {
+            dot.className = 'slider__slide-dots';
+        }
         dot.setAttribute('index', i)
         dotHolder.appendChild(dot);
 
         dot.addEventListener('click', (e) => {
-            
+            console.log(e.target);
+            Array.from(document.querySelectorAll('.active-dot')).forEach((el) => el.classList.remove('active-dot'));
+            e.target.classList.add('active-dot');
+            let activeElement = e.target.getAttribute("index")
+            for(let i = 0; i < slides.length; i++) {
+                slides[i].classList.remove('active');
+            };
+            slides[activeElement].classList.add('active');
+        })
+    }
+}
+
+// ------------------------------
+// ----- POP-UP SLIDE -----
+// ------------------------------
+const popupDotHolder = document.querySelector('.popup-dot-holder');
+const popupSliderItems = document.querySelector('.slider-popup__items');
+const popupSlides = document.querySelector('.slider-popup__items').children;
+
+let popupImages = [];
+
+popupImages[0] = './assests/slides-popup/slide4.png';
+popupImages[1] = './assests/slides-popup/slide3.png';;
+popupImages[2] = './assests/slides-popup/slide2.png';
+popupImages[3] = './assests/slides-popup/slide1.png';
+
+let popupIndex = 0;
+let popupDotCount = popupImages.length;
+
+window.addEventListener('DOMContentLoaded', function() {
+    handlePopupSlider();
+    handlePopopDots(popupDotCount);
+});
+
+function handlePopupSlider() {
+    let slider = popupImages.map(function(popupImage) {
+        return `
+            <div class="item">
+                <img src="${popupImage}"/>
+            </div>
+        `
+    });
+    slider = slider.join('');
+    popupSliderItems.innerHTML = slider;
+
+    for(let i = 0; i < popupSlides.length; i++) {
+        popupSlides[i].classList.remove('active');
+    };
+    popupSlides[index].classList.add('active');
+};
+
+function handlePopopDots(popupDotCount) {
+    for(let i = 0; i < popupDotCount; i++) {
+        let dot = document.createElement('div');
+        if(i === 0) {
+            dot.className = 'slider-popup__slide-dot active-popup-dot';
+        } else {
+            dot.className = 'slider-popup__slide-dot';
+        }
+        dot.setAttribute('index', i)
+        popupDotHolder.appendChild(dot);
+
+        dot.addEventListener('click', (e) => {
+            console.log(e.target);
+            Array.from(document.querySelectorAll('.active-popup-dot')).forEach((el) => el.classList.remove('active-popup-dot'));
+            e.target.classList.add('active-popup-dot');
+            let activeElement = e.target.getAttribute("index")
+            for(let i = 0; i < popupSlides.length; i++) {
+                popupSlides[i].classList.remove('active');
+            };
+            popupSlides[activeElement].classList.add('active');
         })
     }
 }
